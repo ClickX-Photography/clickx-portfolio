f0f8a6427ea276a9d1a73246059b0c3b240faf0c
AnushkaX <anushka.mutusinghe@gmail.com>
2024-05-24 23:58:36 +0530

    fix build blockers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import Header from './components/Header';
+// import Header from './components/Header';
 import AnimRoutes from './components/AnimRoutes';
 
 import { BrowserRouter as Router } from 'react-router-dom';
